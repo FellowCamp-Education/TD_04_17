2d434885567ea69c6c070dca13931a167dbfee79
Angelo Encarnacion <angeloe@nait.ca>
2023-04-18 11:48:37 -0600

    Creating Towers only on Valid areas.

--- Assets/ClickControl.cs
+++ Assets/ClickControl.cs
@@ -6,7 +6,7 @@ using Unity.AI.Navigation;
 public class ClickControl : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] GameObject TowerPrefab;
-    [SerializeField] GameObject MinePrefab;
+
     int enemyPathMask = 1 << 3;
     int towerMask = 1 << 4;
 
@@ -17,10 +17,6 @@ public class ClickControl : MonoBehaviour, IPointerClickHandler
         if (hit.mask == towerMask && eventData.button == PointerEventData.InputButton.Left)
         {
             Instantiate(TowerPrefab, hit.position, Quaternion.identity);
-        }
-        else if (hit.mask == enemyPathMask && eventData.button == PointerEventData.InputButton.Right)
-        {
-            Instantiate(MinePrefab, hit.position, Quaternion.identity);
         }
     }
 }
