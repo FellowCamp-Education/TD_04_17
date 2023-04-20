c15c068320de5319788faf9c3efecf2e7338654e
Angelo Encarnacion <angeloe@nait.ca>
2023-04-20 09:28:12 -0600

    misc updates

--- Assets/ClickControl.cs
+++ Assets/ClickControl.cs
@@ -6,6 +6,7 @@ using Unity.AI.Navigation;
 public class ClickControl : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] GameObject TowerPrefab;
+    [SerializeField] GameObject MinePrefab;
 
     int enemyPathMask = 1 << 3;
     int towerMask = 1 << 4;
@@ -13,10 +14,15 @@ public class ClickControl : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         NavMeshHit hit;
+        RaycastResult raycastHit = eventData.pointerCurrentRaycast;
         NavMesh.SamplePosition(eventData.pointerCurrentRaycast.worldPosition, out hit, 0.1f, NavMesh.AllAreas);
         if (hit.mask == towerMask && eventData.button == PointerEventData.InputButton.Left)
         {
             Instantiate(TowerPrefab, hit.position, Quaternion.identity);
+        }
+        else if (hit.mask == enemyPathMask && eventData.button == PointerEventData.InputButton.Right)
+        {
+            Instantiate(MinePrefab, hit.position, Quaternion.identity);
         }
     }
 }
